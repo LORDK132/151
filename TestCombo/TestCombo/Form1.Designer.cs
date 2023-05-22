996f21c8b35a9511ea04ab41f504d7578ac27f97
LORDK132 <96495690+LORDK132@users.noreply.github.com>
2023-05-22 11:31:24 +0100

    Add files via upload

---
+++ TestCombo/TestCombo/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace TestCombo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cob = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.text = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cob
+            // 
+            this.cob.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cob.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cob.FormattingEnabled = true;
+            this.cob.Items.AddRange(new object[] {
+            "kamal",
+            "saladin",
+            "fatinazahra",
+            "abd arhman"});
+            this.cob.Location = new System.Drawing.Point(33, 12);
+            this.cob.Name = "cob";
+            this.cob.Size = new System.Drawing.Size(530, 33);
+            this.cob.TabIndex = 0;
+            this.cob.SelectedIndexChanged += new System.EventHandler(this.cob_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(62, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // text
+            // 
+            this.text.Location = new System.Drawing.Point(138, 127);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(248, 31);
+            this.text.TabIndex = 2;
+            this.text.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(612, 177);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cob);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "Form1";
+            this.Text = "ADD";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cob;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox text;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TestCombo/TestCombo/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestCombo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cob.Items.Add(text.Text);
+
+        }
+
+        private void addstring_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void cob_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

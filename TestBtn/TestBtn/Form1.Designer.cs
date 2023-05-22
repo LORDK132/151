20b3c5ea9558732747f1dd620093d36917e04b8f
LORDK132 <96495690+LORDK132@users.noreply.github.com>
2023-05-22 11:30:37 +0100

    Add files via upload

---
+++ TestBtn/TestBtn/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace TestBtn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ok = new System.Windows.Forms.Button();
+            this.text1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ok
+            // 
+            this.ok.Location = new System.Drawing.Point(136, 137);
+            this.ok.Name = "ok";
+            this.ok.Size = new System.Drawing.Size(272, 44);
+            this.ok.TabIndex = 0;
+            this.ok.Text = "clik hier";
+            this.ok.UseVisualStyleBackColor = true;
+            this.ok.Click += new System.EventHandler(this.ok_Click);
+            this.ok.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ok_MouseDown);
+            this.ok.MouseLeave += new System.EventHandler(this.ok_MouseLeave);
+            this.ok.MouseHover += new System.EventHandler(this.ok_MouseHover);
+            // 
+            // text1
+            // 
+            this.text1.Location = new System.Drawing.Point(136, 102);
+            this.text1.Name = "text1";
+            this.text1.Size = new System.Drawing.Size(272, 29);
+            this.text1.TabIndex = 1;
+            this.text1.Visible = false;
+            this.text1.TextChanged += new System.EventHandler(this.text1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(570, 319);
+            this.Controls.Add(this.text1);
+            this.Controls.Add(this.ok);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ok;
+        private System.Windows.Forms.TextBox text1;
+    }
+}
+
---
+++ TestBtn/TestBtn/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestBtn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ok_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hi "+text1.Text);
+
+        }
+
+        private void ok_MouseHover(object sender, EventArgs e)
+        {
+            text1.Visible = true;
+            text1.Focus();
+        }
+
+        private void ok_MouseLeave(object sender, EventArgs e)
+        {
+            text1.Visible = false;
+            
+        }
+
+        private void ok_MouseDown(object sender, MouseEventArgs e)
+        {
+          
+        }
+
+        private void text1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
